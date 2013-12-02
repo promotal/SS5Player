752daf17bfb6c86740d8130daaf20022c194dcf2
Takuya Sugimoto <sugimoto@promotal.co.jp>
2013-12-02 11:04:49 +0900

    asdoc generate script

---
+++ gen_asdoc.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+aasdoc \
+	-source-path src \
+	-doc-sources src \
+	-library-path "/Applications/Adobe Gaming SDK 1.2/Frameworks/Starling/Starling-Framework/starling/bin" \
+	-window-title "SS5Player for AS3 API Reference" \
+	-output doc \
